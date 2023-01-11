576985ac600d433d3e630abedf291b3c015c3c37
Kirill Konov <konov.k.i@gmail.com>
2023-01-11 14:12:53 +0300

    exe

--- luneberg.py
+++ luneberg.py
@@ -7,33 +7,18 @@ import pickle
 
 import pandas as pd
 
-
-def Epsilon(Coordinates,ray_freq=0.6,Norm = True):
-    # ang = m.radians(45)
-    # x =
-    x,z = Coordinates[0],-Coordinates[1]
-    koeff = 22.1119   # last 2.21119 * (10 ** 19). included factor (10^9)^2 from freq
-    r_source = -0.938854
-    x2_y2 = x ** 2
-    teta = np.degrees(np.where(z > 0,np.arctan2(np.sqrt(x2_y2),z),0))
-    f_teta = np.where((teta >= 0) & (teta <= 85),2,
-                      np.where((teta > 85) & (teta < 90),-0.1 * teta + 10.5,1))
-    n_teta = np.where((teta >= 0) & (teta <= 20),-0.7 * teta + 32,
-                      np.where((teta > 20) & (teta <= 40),0.2 * teta + 14,
-                               np.where((teta > 40) & (teta <= 60),0.5 * teta + 2,
-                                        np.where((teta > 60) & (teta <= 90),0.23 * teta + 46,1))))
-    p = np.where(z <= 0,0,((np.cos(np.radians(teta / f_teta)) ** n_teta) / ((np.sqrt(x2_y2 + z ** 2) + r_source) ** 2)))
-    wp = koeff * p
-    wp_w =  wp / (ray_freq ** 2)
-    # exp = 1 - wp_w
-    exp = - wp_w # для Tamic
-    return exp
-    # if exp > 0:
-    #     return exp
-    # else:
-    #     return 0  #* dielectric_constant * 10 ** (-12)
-    #
-
+def Epsilon(Coordinates,  Norm=True):
+    x, y= Coordinates[0], Coordinates[1]
+    X_c = 0.75
+    Y_c = 0.75
+    R = 0.5
+    dx = (x - X_c)
+    dy = (y - Y_c)
+    r = np.sqrt(dx ** 2 + dy ** 2)
+    with np.errstate(invalid='ignore'):
+        # par = 0.3 # used for magnetic calculations
+        par = 0
+        return np.where(r <= R, -1+ np.sqrt(2 - (r / R) ** 2) ** 2, par)
 def check_eps():
     x = np.linspace(0,1,100)
     y = np.linspace(0,1,100)
@@ -48,11 +33,11 @@ def set_(name):
     f = open(f'./{name}','a')
     f.write('\n')
     f.close()
-delta = 0.02
-X_boundary = 10#z
-Y_boundary = 0#x
-Xmin = -10#z
-Ymin = -10#x
+delta = 0.002
+X_boundary = 1.5#z
+Y_boundary = 1.5#x
+Xmin = 0#z
+Ymin = 0#x
 print(X_boundary/delta)
 x = np.linspace(Xmin,X_boundary,int(X_boundary/delta))
 y = np.linspace(Ymin,X_boundary,int(X_boundary/delta))
@@ -60,7 +45,7 @@ node = 0
 nX = len(x)
 nY = len(y)
 nPoint = nX*nY
-name = 'fakel.eps'
+name = 'Luneberg.eps'
 f = open(f'./{name}','w')
 f.write('#TamicRTH_planar_DistributionDielectricPermeability_File_V2.00 2000')
 f.close()
@@ -117,7 +102,7 @@ set_(name)
 # f.close()
 count = 0
 N = len(x)*len(y)
-f = open('./fakel.eps','ab')
+f = open(f'./{name}','ab')
 df = pd.DataFrame(columns=y,index=x)
 
 for Yi,Y in enumerate(y):
